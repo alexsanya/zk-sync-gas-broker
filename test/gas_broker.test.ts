9f2227af288c6fc035cdc2a259f485640b882574
alexsanya <alexsanyakoval@gmail.com>
2024-01-01 17:42:42 -0500

    Made it work with USDT permit

--- test/gas_broker.test.ts
+++ test/gas_broker.test.ts
@@ -1,23 +1,22 @@
 import { getWallet, getProvider, deployContract, LOCAL_RICH_WALLETS } from '../deploy/utils';
-import { usdc, fundWithUSDC, getPermitTypedDataHash, splitSignature, getDigestEtalon } from './utils';
+import { usdt, USDT_ADDRESS, fundWithUSDC, fundWithUSDT, getPermitTypedDataHash, splitSignature, getDigestEtalon } from './utils';
 
 const PRICE_ORACLE_ADDRESS = "0xE6E839fec88eFc835F66139f0baC35a596D6d8eD";
-const USDC_ADDRESS = "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4";
-const USDC_VALUE = 100n * 10n**6n;
+const USDT_VALUE = 100n * 10n**6n;
 const TTL = 3600;
 
 describe('GasBrokerTest', function () {
-  it("Should swap USDC to ETH", async () => {
+  it("Should swap USDT to ETH", async () => {
     const signer = getWallet("0xf659966e1562fdec696602ac896ceec313c0572e3707d9606c8f3b470f85e15a");
     const wallet = getWallet(LOCAL_RICH_WALLETS[0].privateKey);
-    // fund with USDC
+    // fund with USDT
     const value = 10n**18n;
-    await fundWithUSDC(wallet, value);
-    const usdcValue = await usdc.balanceOf(wallet.address);
-    await usdc.connect(wallet).transfer(signer.address, usdcValue);
+    await fundWithUSDT(wallet, value);
+    const usdtValue = await usdt.balanceOf(wallet.address);
+    await usdt.connect(wallet).transfer(signer.address, usdtValue);
     console.log('Signer`s address is: ', signer.address);
     console.log('Signer`s ETH balance is: ', (await signer.getBalance()).toString());
-    console.log('Signer`s USDC balance is ', (await usdc.balanceOf(signer.address)).toString());
+    console.log('Signer`s USDT balance is ', (await usdt.balanceOf(signer.address)).toString());
 
 
     //deploy gas broker
@@ -30,69 +29,51 @@ describe('GasBrokerTest', function () {
 
     //prepare permit signature
     const provider = getProvider();
-    const nonce = await usdc.nonces(signer.address);
+    const nonce = await usdt.nonces(signer.address);
     const { timestamp } = await provider.getBlock('latest');
     const message = {
       owner: signer.address,
       spender: gasBroker.address,
-      value: USDC_VALUE,
+      value: USDT_VALUE,
       nonce,
       deadline: timestamp + TTL
     };
-    console.log(message);
+    console.log('Message: ', message);
 
     const domain = {
-      name: await usdc.name(),
+      name: await usdt.name(),
       version: "1",
-      chainId: 324,
-      verifyingContract: USDC_ADDRESS
+      chainId: 260,
+      verifyingContract: USDT_ADDRESS
     }
-
-    const messageHashBytes = ethers.utils.arrayify("0xd12e658b4f72d593ac7e25f9ce92ad3df2a3239a7aa63a9214a403babf9d44bd");
-    console.log(messageHashBytes);
-    const flatSig = await signer.signMessage(messageHashBytes);
-    const sig = ethers.utils.splitSignature(flatSig);
-
-    console.log('Signature: ', sig);
-
+    console.log('Domain: ', domain);
+    const digestEtalon = await getDigestEtalon(message);
 
 
     const digest = await getPermitTypedDataHash(message);
-    const digestEtalon = await getDigestEtalon({
+    const messageHashBytes = ethers.utils.arrayify(digestEtalon);
+    console.log(messageHashBytes);
+    //const flatSig = await signer.signMessage(messageHashBytes);
+    const sig = signer._signingKey().signDigest(digestEtalon);
+    console.log('Signature: ', sig);
 
-        owner: "0x5d2fcc71dFf7182bf49927a2ed3C8FcE0De87723",
-        spender: "0x4B5DF730c2e6b28E17013A1485E5d9BC41Efe021",
-        value: 100000000n,
-        deadline: 1703639975n,
-        nonce: 0
-    });
-    const permitSignature = await signer.signMessage(digestEtalon);
 
     console.log('Digest: ', digest);
     console.log('Digest etalon: ', digestEtalon);
-    console.log('Permit signature: ', permitSignature);
+    console.log('Permit signature: ');
+    console.log(sig);
 
-    const [permitV, permitR, permitS] = splitSignature(permitSignature);
+    await usdt.connect(wallet).permit(
+      message.owner,
+      message.spender,
+      message.value,
+      message.deadline,
+      sig.v,
+      sig.r,
+      sig.s
+    );
 
-    console.log({
-      permitV,
-      permitR,
-      permitS
-    });
-
-      await usdc.connect(wallet).permit(
-        "0x5d2fcc71dFf7182bf49927a2ed3C8FcE0De87723",
-        "0x4B5DF730c2e6b28E17013A1485E5d9BC41Efe021",
-        100000000n,
-        1703639975n,
-        "0x1c",
-        "0x09bfaa3103d5bee140ca7062e8cd02e0560a4e35852b7ad3a45a54ff3b2aae79",
-        "0x1526153f1f1ca1008c9b70f4f283d3c8b36694db721f167f0c6c4162a05061a0"
-      );
-
- //   await usdc.connect(wallet).permit(message.owner, message.spender, message.value, message.deadline, permitV, permitR, permitS);
-
-    const nonceAfter = await usdc.nonces(signer.address);
+    const nonceAfter = await usdt.nonces(signer.address);
     console.log('Nonce after: ', nonceAfter);
 
     // lets change 100 USDC to ETH
--- test/utils.ts
+++ test/utils.ts
@@ -8,17 +8,19 @@ import erc20abi from '../abis/erc20.json';
 const SYNC_SWAP_CLASSIC_POOL_FACTORY = "0xf2DAd89f2788a8CD54625C60b55cD3d2D0ACa7Cb";
 const WETH_ADDRESS = "0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91";
 const USDC_ADDRESS = "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4";
+export const USDT_ADDRESS = "0x493257fD37EDB34451f62EDf8D2a0C418852bA4C";
 const ROUTER_ADDRESS = "0x2da10A1e27bF85cEdD8FFb1AbBe97e53391C0295";
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 export const usdc = new ethers.Contract(USDC_ADDRESS, erc20abi, getProvider());
+export const usdt = new ethers.Contract(USDT_ADDRESS, erc20abi, getProvider());
 const PERMIT_TYPEHASH = "0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9";
 
 
 
 export async function getDigestEtalon(message) {
   const wallet = getWallet(LOCAL_RICH_WALLETS[0].privateKey);
-  const domainSeparator = await usdc.DOMAIN_SEPARATOR();
+  const domainSeparator = await usdt.DOMAIN_SEPARATOR();
   console.log('[getDigestEtalon] Domain separator:', domainSeparator);
 
   //deploy permitSigUtils
@@ -27,6 +29,8 @@ export async function getDigestEtalon(message) {
     [domainSeparator],
     { wallet, silent: true }
   );
+
+  console.log('PermitSig utils deployed, Address: ', permitSigUtils.address);
 
   return await permitSigUtils.getTypedDataHash(message);
 
@@ -60,7 +64,7 @@ function getStructHash(permitMessage) {
 }
 
 export async function getPermitTypedDataHash(permitMessage) {
-  const domainSeparator = await usdc.DOMAIN_SEPARATOR();
+  const domainSeparator = await usdt.DOMAIN_SEPARATOR();
   const structHash = getStructHash(permitMessage);
   return ethers.utils.keccak256(
     ethers.utils.concat([
@@ -86,7 +90,6 @@ export async function fundWithUSDC(wallet, value) {
     if (poolAddress === ZERO_ADDRESS) {
         throw Error('Pool not exists');
     }
-
 
     // Gets the reserves of the pool.
     const pool = new ethers.Contract(poolAddress, classicPoolAbi, getProvider());
@@ -139,5 +142,75 @@ export async function fundWithUSDC(wallet, value) {
     await response.wait();
 
     const balanceAfter = await usdc.balanceOf(wallet.address);
-
 }
+
+
+export async function fundWithUSDT(wallet, value) {
+    const classicPoolFactory = new ethers.Contract(
+        SYNC_SWAP_CLASSIC_POOL_FACTORY,
+        classicPoolFactoryAbi,
+        getProvider()
+    );
+
+    // Gets the address of the ETH/DAI Classic Pool.
+    // wETH is used internally by the pools.
+    const poolAddress: string = await classicPoolFactory.getPool(WETH_ADDRESS, USDT_ADDRESS);   
+
+    // Checks whether the pool exists.
+    if (poolAddress === ZERO_ADDRESS) {
+        throw Error('Pool not exists');
+    }
+
+    // Gets the reserves of the pool.
+    const pool = new ethers.Contract(poolAddress, classicPoolAbi, getProvider());
+    const reserves = await pool.getReserves(); // Returns tuple (uint, uint)
+
+    // Sorts the reserves by token addresses.
+    const [reserveETH, reserveUSDT] = WETH_ADDRESS < USDT_ADDRESS ? reserves : [reserves[1], reserves[0]];
+
+    // The input amount of ETH
+
+    const withdrawMode = 1; // 1 or 2 to withdraw to user's wallet
+
+    const swapData: string = ethers.utils.defaultAbiCoder.encode(
+        ["address", "address", "uint8"],
+        [WETH_ADDRESS, wallet.address, withdrawMode], // tokenIn, to, withdraw mode
+    );
+
+    // We have only 1 step.
+    const steps = [{
+        pool: poolAddress,
+        data: swapData,
+        callback: ZERO_ADDRESS, // we don't have a callback
+        callbackData: '0x',
+    }];
+
+    const nativeETHAddress = ZERO_ADDRESS;
+
+    // We have only 1 path.
+    const paths = [{
+        steps: steps,
+        tokenIn: nativeETHAddress,
+        amountIn: value,
+    }];
+
+    const balanceBefore = await usdt.balanceOf(wallet.address);
+
+    const router = new ethers.Contract(ROUTER_ADDRESS, routerAbi, getProvider());
+
+    // Note: checks approval for ERC20 tokens.
+    // The router will handle the deposit to the pool's vault account.
+    const response = await router.connect(wallet).swap(
+        paths, // paths
+        0, // amountOutMin // Note: ensures slippage here
+        BigInt(Math.floor(Date.now() / 1000)) + 1800n, // deadline // 30 minutes
+        {
+            value: value,
+        }
+    );
+
+    await response.wait();
+
+    const balanceAfter = await usdt.balanceOf(wallet.address);
+}
+
